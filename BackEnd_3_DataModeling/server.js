f6454dd6f590bd19d892f540dadb70203948a943
ErGovindthakur <ergovindthakur@gmail.com>
2024-10-30 18:56:47 +0530

    Adding database file here

--- BackEnd_3_DataModeling/server.js
+++ BackEnd_3_DataModeling/server.js
@@ -1,17 +1,24 @@
 // Creating our server here
 
-import express from 'express'
+// import express from 'express'
 
-const app = new express()
+// const app = new express()
 
-const port = process.env.PORT || 7000
+// const port = process.env.PORT || 7000
 
 // app.use(express.static('dist'))
 
-app.get('/', (req,res) => {
-     res.send('This is default Route')
-})
+// app.get('/', (req,res) => {
+//      res.send('This is default Route')
+// })
 
-app.listen(port, () => {
-     console.log(`server is running at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//      console.log(`server is running at http://localhost:${port}`)
+// })
+import dotenv from "dotenv"
+
+import connectDb from "./src/db/db.js"
+
+dotenv.config({path:"./env"})
+
+connectDb()
--- BackEnd_3_DataModeling/src/db/db.js
+++ BackEnd_3_DataModeling/src/db/db.js
@@ -1,11 +1,15 @@
 // Setting up our data base connection
 
 import mongoose from 'mongoose'
+import {dbName} from '../constants.js'
 
 const connectDb = async() => {
      try {
-          const response = await mongoose.connect(``)
+          const response = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`)
+          console.log("Mongo db host !! connected -: ",response.connection.host)
      } catch (error) {
           console.log(`Database connection failed from db.js ${error.message}`)
      }
 }
+
+export default connectDb
