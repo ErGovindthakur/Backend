b69ae5f61133bec228d22a1d885ee1d0391dad05
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-03 18:26:59 +0530

    add like tweet and playlist model

---
+++ VideoTubeBackendProduct/src/models/like.models.js
@@ -0,0 +1,25 @@
+// creating a like model
+
+import mongoose, { Schema } from "mongoose";
+
+
+const likeSchema = new Schema({
+     video:{
+          type:Schema.Types.ObjectId,
+          ref:"Video"
+     },
+     comment:{
+          type:Schema.Types.ObjectId,
+          ref:"Comment"
+     },
+     tweet:{
+          type:Schema.Types.ObjectId,
+          ref:"Tweet"
+     },
+     likedBy:{
+          type:Schema.Types.ObjectId,
+          ref:"User"
+     },
+},{timestamps:true});
+
+export const Like = mongoose.model("Like",likeSchema)
