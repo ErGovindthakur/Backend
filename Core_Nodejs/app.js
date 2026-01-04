995d728c764d0fc3986e11510345abbffb546c36
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-04 12:05:02 +0530

    build basic greeting app in core nodejs

--- Core_Nodejs/app.js
+++ Core_Nodejs/app.js
@@ -1,3 +1,23 @@
 import process from "process";
 console.log(process.argv)
+
+/*
+since we are using here -:
+ process.argv[2], process.argv[3] (not 0,1)
+
+ bcz nodejs (process.argv) always returns an array already occupied index 0,1 
+ (0th index contains=> .exe file path), 
+ (1st index contains => your actual file path of code)
+*/
+
+
+/*
+eg -> 
+console.log(process.argv)
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\HP\\Documents\\BackEnd_With_Hitesh\\Core_Nodejs\\app.js'
+]
+=> 
+*/
 // console.log(process.argv[2],process.argv[3])
