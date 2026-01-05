a8880269d998b767f2551fe745987de2ae1629b7
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-05 11:52:46 +0530

    explore in build module in nodejs

--- Core_Nodejs/01_Node_Modules/02_InbuiltModule.js
+++ Core_Nodejs/01_Node_Modules/02_InbuiltModule.js
@@ -13,4 +13,18 @@ const hours = new Date().getHours();
 
 const greet = getGreetings(hours);
 
-console.log(`${greet}, ${name}`)
+// console.log(`${greet}, ${name}`)
+
+
+// Exploring inbuilt module
+
+import os from "node:os"
+
+// console.log("CPUs",os.cpus()) // cpu info
+// console.log("CPUs",os.cpus().length) // total core
+console.log("Total Memory ",Math.floor(os.totalmem()/(1024*1024*1024))) // total memory in GB
+console.log("Free Memory ",Math.floor(os.freemem()/(1024*1024*1024))) // total free memory in GB
+console.log("Uptime ",Math.floor(os.uptime()/(60*60)));
+console.log("Hostname ", os.hostname())
+console.log("User Info ", os.userInfo())
+console.log("Machine Architecture ", os.machine())
