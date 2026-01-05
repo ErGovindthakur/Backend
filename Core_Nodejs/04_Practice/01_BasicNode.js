e4e2aab4f75e9fca5a09e2a45e6d423f98654e88
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-05 22:37:55 +0530

    basic practice of process module of nodejs

---
+++ Core_Nodejs/04_Practice/01_BasicNode.js
@@ -0,0 +1,18 @@
+// writing simple code using nodejs
+
+import process from "process";
+
+let hours = new Date().getHours();
+
+let name = process.argv[2];
+
+export const getGreeting = (hours) => {
+     if(hours < 3 || hours > 19) return "Good Night!";
+     if(hours < 11) return "Good Morning!";
+     if(hours < 16) return "Good Afternoon!";
+     return "Good Evening!"
+}
+
+let greet = getGreeting(hours);
+
+console.log(`${greet},${name}`)
