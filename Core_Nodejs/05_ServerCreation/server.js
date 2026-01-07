13afbf2adec3aff5e6f649bd947917fa1c3c8e12
ErGovindthakur <ergovindthakur@gmail.com>
2026-01-08 00:27:57 +0530

    explore routing in nodejs

--- Core_Nodejs/05_ServerCreation/server.js
+++ Core_Nodejs/05_ServerCreation/server.js
@@ -1,9 +1,31 @@
 import {createServer} from "node:http"
 
 const server = createServer((req,res)=>{
-     console.log("Request Received -> ")
+     console.log("Request Received... ")
 
-     res.end("Hello from nodejs server");
+     // 1. sending a normal response form server
+     // res.end("Hello from nodejs server");
+
+     // 2. sending normal html file
+     // res.writeHead(200,{
+     //      'content-type':'text/html'
+     // })
+     // res.end('<h1>Hello Nodejs</h1>')
+
+
+     // 3. Exploring the concept of routing in core nodejs
+     if(req.url === "/"){
+          res.writeHead(200,{
+               'content-type':'text/html'
+          })
+
+          res.end('Hello from Home page')
+     }else if(req.url === "/about"){
+          res.writeHead(200,{
+               'content-type':'text/html'
+          })
+          res.end('Hello from About page')
+     }
 })
 
 const port = 9000;
